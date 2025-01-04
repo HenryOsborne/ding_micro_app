5ed3dfdeccd9eea4d626f26ff0e713c39c58a5cb
王凯 <wk1009519920@gmail.com>
2025-01-04 10:46:15 +0800

    init

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,5 +1,6 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import NotFoundComponent from "@/views/NotFoundComponent.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,6 +18,14 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
+    {
+      path: '/warning',
+      name: 'warning',
+      component: NotFoundComponent
+    },
+    {path: '/404', name: '404', component: NotFoundComponent},
+    {path: '/405', name: '405', component: NotFoundComponent},
+    {path: '/warning', name: 'warning', component: NotFoundComponent},
   ],
 })
 
--- front/vite.config.js
+++ front/vite.config.js
@@ -1,28 +1,34 @@
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from 'node:url'
 
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
-import { VantResolver } from '@vant/auto-import-resolver';
+import {VantResolver} from '@vant/auto-import-resolver';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    AutoImport({
+      resolvers: [VantResolver()],
+    }),
+    Components({
+      resolvers: [VantResolver()],
+    }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  server:{
-    host:'0.0.0.0',
-    headers:{
-      'Access-Control-Allow-Origin':'*'
+  server: {
+    host: '0.0.0.0',
+    headers: {
+      'Access-Control-Allow-Origin': '*'
     }
   }
 })
