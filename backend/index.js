fa18df26f981cd051b3d200e17f3327ef84c375d
王凯 <wk1009519920@gmail.com>
2025-01-04 11:43:41 +0800

    update backend

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors'
+import { getLocalIP } from "./utils/wking.js"
+import dingdingRouter from './router/dingding.js'
+
+import famous_words from "./datas/famous_words.json" with { type: "json" };
+const port = 3000;
+
+const app = express();
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static("./public"))
+app.use("/dingding", dingdingRouter)
+// app.use("/wechat",xxxx)
+app.listen(port, () => {
+  const words = famous_words[Math.floor(Math.random() * famous_words.length)];
+  console.log(` running：http://${getLocalIP()}:${port} 🥬`)
+  console.log(`🍵 ${words} 🥕`)
+})
---
+++ backend/router/dingding.js
@@ -0,0 +1,19 @@
+import {Router} from "express";
+import {dingJsApiTicket, dingToken} from "../middle/index.js"
+import dingController from "../controller/dingController.js";
+
+let router = Router();
+router.get("/", (req, res) => {
+  res.send({
+    code: 200,
+    data: "你好钉钉"
+  })
+})
+
+// jssdk鉴权的接口
+router.get("/jsSdkAuthorized", dingJsApiTicket, dingController.jsSdkAuthroized)
+// 获取钉钉用户信息
+router.post("/getUserInfo", dingToken, dingController.getUserInfo)
+
+
+export default router;
